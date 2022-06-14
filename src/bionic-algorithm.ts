15c7793fb4acd73bc9803680bf33189920f002f9
Ekku Leivonen <ekku@Ekkus-MBP.lan>
2022-06-14 12:46:24 +0100

    working bionic demo

--- src/bionic-algorithm.ts
+++ src/bionic-algorithm.ts
@@ -1,21 +1,56 @@
 export {replaceNode};
 
-const replaceNode = (node: ChildNode)=>{
+const replaceNode = (node: ChildNode): HTMLSpanElement | ChildNode =>{
     if (node.textContent === null) return node;
-    const newNode = document.createElement("span");
+    const newNode: HTMLSpanElement = document.createElement("span");
     const words: string[] = node.textContent.split(" ").filter(word => word!=="" && word !== "\n");
-    const HTMLstring: string = spannify(words);
+    const HTMLstring: string = spannifyChild(words);
     newNode.innerHTML = HTMLstring;
     return newNode;
 };
 
-const spannify = (words : string[]) : string=>{
-    let HTMLstring = "";
+const spannifyChild = (words : string[]): string =>{
+    let HTMLstring: string = "";
     words.forEach(word =>{
-        const span = `<span> ${word} </span>`;
-        HTMLstring += span;
+        const biometricWord = runBio(word);
+        const spannifiedWord = `<span> ${runBio(word)} </span>`;
+        HTMLstring += spannifiedWord;
     });
     return HTMLstring;
+};
+
+const runBio = (word: string): string =>{
+    var hasNumber = /\d/;
+    if(hasNumber.test(word))return word;   
+    let biometricWord = "";
+    const characters: string[] = word.split("");
+    switch (characters.length) {
+        case 0:
+            biometricWord = word;
+            break;
+        case 1:
+            biometricWord = `<span style="font-weight: bold;"> ${word} </span>`;
+            break;
+        case 2:
+            biometricWord = `<span style="font-weight: bold;"> ${word} </span>`;
+            break;
+        case 3:
+            biometricWord = `<span style="font-weight: bold;"> ${word} </span>`;
+            break;
+        default:
+            let bold = "";
+            let notBold = "";
+            for (let i = 0; i < characters.length; i++){
+                if (i < characters.length/2){
+                    bold += characters[i];
+                }else{
+                    notBold += characters[i];
+                }
+            }
+            biometricWord = `<span style="font-weight: bold;"> ${bold}</span>${notBold} `;
+            break;
+    }
+    return biometricWord;
 };
 
 
